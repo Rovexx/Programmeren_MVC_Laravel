df445cda756424538bae7f9ebe59b73ed0d98ca0
Rovexx <roel.versteeg@hotmail.com>
2018-10-23 17:16:16 +0200

    Front end styling voor log in

--- website/resources/views/auth/login.blade.php
+++ website/resources/views/auth/login.blade.php
@@ -1,71 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+    <div class="row center">
+        <H1>Log in</H1>
+        <h5>Vul de juiste gegevens in</h5>
+    </div>
+    <div class="row">
+        <div class="col s6 offset-s3 z-depth-1">
+            <br><br>
+            <form action="{{ route('login') }}" method="POST">
+                <!-- CSRF protection -->
+                @csrf
+                <!-- E-mail address -->
+                <div class="row">
+                    <div class="input-field col s10 offset-s1">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="email" name="email" type="email" class="validate form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ old('email') }}" required autofocus>
+                        <label for="email">{{ __('E-Mail Address') }}</label>
+                    </div>
                 </div>
-            </div>
+                <!-- Password -->
+                <div class="row">
+                    <div class="input-field col s10 offset-s1">
+                        <i class="material-icons prefix">https</i>
+                        <input id="password" name="password" type="password" class="validate form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required>
+                        <label for="wachtwoord">{{ __('Password') }}</label>
+                    </div>
+                </div>
+                <!-- remember me -->
+                <div class="row">
+                    <div class="col s10 offset-s1">
+                        <label for="remember" class="rememberMe">
+                            <input id="remember" name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }} class="filled-in"/>
+                            <span>{{ __('Remember Me') }}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- submit -->
+                    <div class="col s6 offset-s3">
+                        <button class="col s12 btn waves-effect waves-light amber" type="submit" name="submit">{{ __('Login') }}
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
+                    <br><br>
+                    <!-- forgot password -->
+                    <div class="col s6 offset-s3">
+                        <a class="grey-text" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                    </div>
+                </div>
+            </form>
+            <br>
         </div>
     </div>
-</div>
 @endsection
--- website/resources/views/inc/messages.blade.php
+++ website/resources/views/inc/messages.blade.php
@@ -2,6 +2,7 @@
     <!-- ->all() erbij omdat errors een object is -->
     @foreach ($errors->all() as $error)
         <script>M.toast({html: "{{ $error }}", classes: 'red'})</script>
+        <script>console.log("first error")</script>
     @endforeach
 @endif
 
@@ -13,7 +14,7 @@
     <script>M.toast({html: "{{ Session('error') }}", classes: 'red' })</script>
 @endif
 
+<!-- logged in status -->
 @if (Session::has('status'))
     <script>M.toast({html: "{{ Session('status') }}", classes: 'green' })</script>
 @endif
-
