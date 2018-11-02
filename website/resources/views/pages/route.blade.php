865cdfc06654e25392f8db7713551fca1b909c50
Rovexx <roel.versteeg@hotmail.com>
2018-11-02 20:57:39 +0100

    Route pagina gemaakt
    - Google maps embed api
    - Route text

--- website/resources/views/pages/route.blade.php
+++ website/resources/views/pages/route.blade.php
@@ -1,6 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello World</h1>
-    <h5>Dit is de route pagina</h5>
+    <div class="row">
+        <br>
+        <div class="col s10 offset-s1 z-depth-1 white">
+            <h3 class="bold center">Route</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s10 offset-s1 z-index-1 white">
+            <div class="col s6">
+                <h5 class="bold">Afrit 26 vanaf Gorinchem</h5><br>
+                <h6 class="flow-text">Als u op rijksweg A15/E31 komend vanuit de richting Gorinchem afrit 26 naar Hardinxveld-Giessendam/Giessenburg neemt, 
+                    dan bevindt u zich aan het einde van de afrit al op de Nieuweweg.<br>
+                    Sla onder aan de afslag rechtsaf. Na ± 500m vindt u de autohandel aan uw linkerzijde.
+                </h6>
+                <br>
+                <hr>
+                <br>
+                <h5 class="bold">Afrit 26 vanaf Rotterdam</h5>
+                <h6 class="flow-text">Als u op rijksweg A15/E31 komend vanuit de richting Rotterdam afrit 26 naar Hardinxveld-Giessendam/Giessenburg neemt, 
+                    dan bevindt u zich aan het einde van de afrit al op de Nieuweweg.<br>
+                    Sla vervolgens onder aan de afslag linkssaf. Na ± 500m vindt u de autohandel aan uw linkerzijde.<br>
+                    Afrit 25 (Hardinxveld-Giessendam west/Sliedrecht oost) vanaf Gorinchem
+                    Aan het einde van de afslag komt u op een rotonde.<br>
+                    Neem hier de eerste afslag.  zie verder (*)
+                </h6>
+                <br>
+                <hr>
+                <br>
+                <h5 class="bold">Afrit 25 (Hardinxveld-Giessendam west/Sliedrecht oost) vanaf Rotterdam</h5>
+                <h6 class="flow-text">Ga aan het einde van de afslag (verkeerslichten) linksaf;<br>
+                    het viaduct over en bij de verkeerslichten rechtsaf. U komt dan op een rotonde, waarvan u de tweede afslag neemt (rechtdoor).<br>
+                    U bevindt zich nu op de Peulenlaan.<br>
+                    Rij ongeveer 3km rechtdoor tot de verkeerslichten en sla daar  linksaf.<br>
+                    U bevindt zich dan op de Nieuweweg. Na ±400 m vindt u de autohandel aan uw linkerzijde.
+                </h6>
+                <br>
+            </div>
+            <br>
+            <div class="col s6">
+                <iframe class="z-depth-2"
+                    width="100%"
+                    height="900px"
+                    frameborder="0" style="border:0"
+                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBXNQYjd9lVLi87dOCgcnwx-XPGaPQqzE4
+                    &q=Autohandel+De+Voorbijgang" allowfullscreen>
+                </iframe>
+            </div>
+        </div>
+    </div>
 @stop
